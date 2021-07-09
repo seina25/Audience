abda05797a4ddee9276e779eff9e762ea6cade5c
seina25 <replace421@gmail.com>
2021-07-09 06:02:06 +0000

    [Add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords'
+  }
+  
+  devise_for :members, controllers:{
+    sessions: 'members/sessions',
+    passwords: 'members/passwords',
+    registrations: 'members/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
