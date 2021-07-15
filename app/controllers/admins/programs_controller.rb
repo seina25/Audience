0ccacf43bc2b5be6034f62ace2da86433e593cb6
seina25 <replace421@gmail.com>
2021-07-15 18:55:43 +0000

    [Update]program_data

--- app/controllers/admins/programs_controller.rb
+++ app/controllers/admins/programs_controller.rb
@@ -22,17 +22,12 @@ class Admins::ProgramsController < ApplicationController
     driver.get('https://tv.yahoo.co.jp/listings')
     sleep(rand(5))
 
-    # 番組表からurl取得
-    begin
+    # 番組表からurl一件ずつ取得
     @programs = []
     elements = driver.find_elements(:class, "listingTablesTextLink")
     @urls = elements.map { |element| element.attribute('href') }
     @urls.first(2).each do |url|
-      p url
     driver.navigate.to(url)
-    # d = Selenium::WebDriver.for :chrome, options: options
-    # d.manage.timeouts.implicit_wait = @timeout
-    # wait = Selenium::WebDriver::Wait.new(timeout: @wait_time)
 
     sleep(rand(5))
 
@@ -40,28 +35,68 @@ class Admins::ProgramsController < ApplicationController
     p cur_url
 
     # 番組データを取得
+    begin
     title = driver.find_element(:class, "programRatingContentTitle").text
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+    title = ""
+    end
+
+    begin
     second_title = driver.find_element(:class, "programVideoContentTitleText").text
-    cast1 = driver.find_element(:xpath, "//*[@id='__next']/div/main/article/div/div[7]/div[2]/section[3]/p").text
-    cast2 = driver.find_element(:xpath, "//*[@id='__next']/div/main/article/div/div[7]/div[2]/section[2]/p").text
-    cast3 = driver.find_element(:class, "programCastInformationList").text
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+    second_title = ""
+    end
+
+    begin
+    cast = driver.find_element(:xpath, "//h3[contains(text(), '出演者')]/following-sibling::p[1]").text
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+    cast = ""
+    end
+
+    begin
     channel = driver.find_element(:class, "channelText").text
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+    channel = ""
+    end
+
+    begin
     category = driver.find_element(:class, "programOtherDataListText").text
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+    category = ""
+    end
+
     # 日付
-    date = driver.find_element(:xpath, "//*[@id='__next']/div/main/article/div/div[6]/div/div/div/time[1]/span[1]").text
+    begin
+    date = driver.find_element(:xpath, "//p[contains(text(), '放送日時・内容')]/following-sibling::div[1]//span[1]").text
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+    date = ""
+    end
+
     # 開始時間
-    start_time = driver.find_element(:xpath, "//*[@id='__next']/div/main/article/div/div[6]/div/div/div/time[1]/span[3]").text
+    begin
+    start_time = driver.find_element(:xpath, "//p[contains(text(), '放送日時・内容')]/following-sibling::div[1]//span[3]").text
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+    start_time = ""
+    end
+
     # 終了時間
+    begin
     end_time = driver.find_element(:class, "scheduleTextTimeEnd").text
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+    end_time = ""
+    end
+
     # 曜日
+    begin
     by_weekday = driver.find_element(:class, "scheduleTextWeek").text
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+    by_weekday = ""
+    end
 
     # save入れる
     p title
     p second_title
-    p cast1
-    p cast2
-    p cast3
+    p cast
     p channel
     p category
     p date
@@ -69,14 +104,8 @@ class Admins::ProgramsController < ApplicationController
     p end_time
     p by_weekday
 
-    @programs.push({ 'title': title, 'second_title': second_title, 'cast1': cast1, 'cast2': cast2, 'cast3': cast3,
-    'channel': channel, 'category': category, 'date': date, 'start_time': start_time, 'end_time': end_time, 'by_weekday': by_weekday, })
-
-    p @program
-    rescue Selenium::WebDriver::Error::NoSuchElementError
-    p 'no such element error!!'
-
-    end
+    @programs.push({ 'title': title, 'second_title': second_title, 'cast': cast, 'channel': channel,
+    'category': category, 'date': date, 'start_time': start_time, 'end_time': end_time, 'by_weekday': by_weekday })
 
     # 番組名を取得
     # @title = driver.find_elements(:class, "programListItemTitleLink")
@@ -94,10 +123,13 @@ class Admins::ProgramsController < ApplicationController
     # program.title = @titles[i]
     # program.date = @dates[i]
     # program.save
-
+    end
     # ドライバーを閉じる
     driver.quit
-    end
+
+  end
+
+  def spraping
   end
 
   def new
