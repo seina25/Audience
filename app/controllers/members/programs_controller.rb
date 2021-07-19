5ada08de1ca5e090e55b0c9d37dedb7362edbaf6
seina.25 <replace421@gmail.com>
2021-07-19 00:04:04 +0000

    [update]members/program

--- app/controllers/members/programs_controller.rb
+++ app/controllers/members/programs_controller.rb
@@ -1,7 +1,18 @@
 class Members::ProgramsController < ApplicationController
   def index
+    @time = Time.zone.now
+    @programs = Program.all.page(params[:page]).per(20).order(created_at: :desc)
   end
 
   def show
+    @time = Time.zone.now
+    @program = Program.where(id: params[:id])
+  end
+
+  private
+
+  def program_params
+    params.require(:program).permit(:title, :second_title, :category, :cast, :channel,
+    :start_datetime, :end_datetime, :by_weekday, :profile_image_id)
   end
 end
