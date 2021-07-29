e8651f3a0f1a5a41490e7b0bb0ce4480bd75468f
seina.25 <replace421@gmail.com>
2021-07-29 14:58:19 +0000

    [fix]

--- config/schedule.rb
+++ config/schedule.rb
@@ -64,7 +64,7 @@ rescue StandardError => e
 end
 
 # 毎日 日本時間 03:00のスケジューリング
-every 1.day, at: '14:40 pm' do
+every 1.day, at: '15:03 pm' do
   runner 'Batch::ThreedayslaterUpdate.threedayslater_update'
 rescue StandardError => e
   Rails.logger.error('aborted rails runner')
