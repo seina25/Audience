065c1f5b2182735b7a53901d4565c33edc9bc3aa
seina.25 <replace421@gmail.com>
2021-07-29 09:24:16 +0000

    [fix]

--- config/schedule.rb
+++ config/schedule.rb
@@ -72,7 +72,7 @@ every 1.day, at: '16:45 pm' do
 end
 
 # 毎日 日本時間 03:00のスケジューリング
-every 1.day, at: '09:10 am' do
+every 1.day, at: '09:30 am' do
   begin
     runner 'Batch::ThreedayslaterUpdate.threedayslater_update'
   rescue => e
