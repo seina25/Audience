75cfbf754a3174f41740611eb5060a78390e3b21
seina.25 <replace421@gmail.com>
2021-09-02 10:22:02 +0000

    fix

--- app/controllers/members/homes_controller.rb
+++ app/controllers/members/homes_controller.rb
@@ -5,7 +5,5 @@ class Members::HomesController < ApplicationController
     @favorite_top = Program.find(Favorite.group(:program_id).order(Arel.sql('count(program_id) desc')).pluck(:program_id)).first
     @review_top = Program.find(Review.group(:score).order('avg(score) desc').pluck(:program_id)).first
     @pv_top = Program.find(ViewCount.group(:program_id).order(Arel.sql('count(program_id) desc')).pluck(:program_id)).first
-    @recommendation = Program.where(start_datetime: DateTime.now..Float::INFINITY).where('id >= ?',
-                                                                                         rand(Program.first.id..Program.last.id)).first
   end
 end
